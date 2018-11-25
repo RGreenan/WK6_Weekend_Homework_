c3a155e9ac2a3418dec209c82f00b2f2c032bcf6
Ray <ray.greenan.89@outlook.com>
2018-11-25 15:12:44 +0000

    can create new form

--- js/app.js
+++ js/app.js
@@ -1,25 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-});
-//
-// const createNewForm = function (form) {
-//
-//   const newFormItem = document.createElement('ul');
-//   newFormItem.textcontent = 'ul text';
-//   newFormItem.classList.add('new-form-item');
-//   form.appendChild(newFormItem);
-//
-//   const title = document.createElement('h2');
-//   title.textContent = form.title.value;
-//   newformItem.appendChild(title);
-//
-//   const director = document.createElement('h3');
-//   director.textContent = form.director.value;
-//   newformItem.appendChild(director);
-//
-//   const genre = document.createElement('p');
-//   genre.textContent = form.genre.value;
-//   newformItem.appendChild(genre);
-//
-//   return newFormItem;
-// }
+  // CREATE NEW FORM
+  const createNewForm = document.querySelector('#new-item-form');
+  createNewForm.addEventListener('submit', submitNewForm);
+
+  // DELETE FORMS
+  // const deleteAllButton = document.querySelector('#delete-all');
+  // deleteAllButton.addEventListener('click', handleDeleteAllClick);
+})
+
+const submitNewForm = function (event) {
+  event.preventDefault();
+
+  const createNewForm = createNewFormItem(event.target);
+  const movieList = document.querySelector('#movie-list');
+  movieList.appendChild(createNewForm);
+
+  event.target.reset();
+
+
+};
+
+const createNewFormItem = function (form) {
+
+  const newFormItem = document.createElement('ul');
+  newFormItem.classList.add('new-form-item');
+
+  const title = document.createElement('h2');
+  title.textContent = form.title.value;
+  newFormItem.appendChild(title);
+
+  const director = document.createElement('h3');
+  director.textContent = form.director.value;
+  newFormItem.appendChild(director);
+
+  const genre = document.createElement('p');
+  genre.textContent = form.genre.value;
+  newFormItem.appendChild(genre);
+
+  return newFormItem;
+}
